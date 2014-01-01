867c9942bfdf9102070efe02850655d791ae0285
Sultanxda <sultanxda@gmail.com>
2014-01-01 11:53:39 -0800

    pyramid: Increase panel voltage to stock level

--- arch/arm/mach-msm/board-pyramid-display.c
+++ arch/arm/mach-msm/board-pyramid-display.c
@@ -878,7 +878,7 @@ static int mipi_dsi_panel_power(const int on)
 			}
 		}
 
-		rc = regulator_set_voltage(l1_3v, 2900000, 2900000);
+		rc = regulator_set_voltage(l1_3v, 3100000, 3100000);
 		if (rc) {
 			PR_DISP_ERR("%s: error setting l1_3v voltage\n", __func__);
 			return -EINVAL;
